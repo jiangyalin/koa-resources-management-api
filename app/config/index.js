a5d87254f57436e96aa5c612efc4cf1c9c2f3104
蒋亚林 <18725944157@163.com>
2018-02-01 15:05:03 +0800

    xx

---
+++ app/config/index.js
@@ -0,0 +1,10 @@
+// mongodb配置文件
+export default {
+    port: 8088,
+    session: {
+        secret: 'personalDB',
+        key: 'personalDB',
+        maxAge: 2592000000
+    },
+    mongodb: 'mongodb://localhost:27017/personalDB'
+}
--- app/server.js
+++ app/server.js
@@ -1,8 +1,11 @@
-const Koa = require('koa');
-const app = new Koa();
+import Koa from 'koa'
+import config from './config'
+const app = new Koa()
 
 app.use(async ctx => {
-    ctx.body = 'Hello World';
-});
+    ctx.body = 'Hello World2'
+})
 
-app.listen(3000);
+app.listen(config.port, () => {
+    console.log('启动成功：' + config.port)
+})
