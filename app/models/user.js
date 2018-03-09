f5a78f557d50b30c6a707446ec33a5b686dc5334
蒋亚林 <18725944157@163.com>
2018-03-09 18:30:47 +0800

    轻小说

--- app/models/user.js
+++ app/models/user.js
@@ -1,3 +1,12 @@
-/**
- * Created by jiang_pc on 2018/3/9.
- */
+import mongoose from './../lib/db'
+const Schema = mongoose.Schema
+
+// 轻小说模板
+const BookSchema = new Schema({
+    name: { type: String, default: null }, // 用户名
+    password: { type: String, default: null }, // 密码
+    createTime: { type: Date, default: Date.now }, // 创建时间
+    is_deleted: { type: Number, default: 1 } // 删除状态
+})
+
+export default mongoose.model('user', BookSchema)
