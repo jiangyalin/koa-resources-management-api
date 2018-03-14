035fed97110e4dd3e721da2cc1a7a7c8c10b92a5
蒋亚林 <18725944157@163.com>
2018-03-14 17:53:39 +0800

    添加账户，编辑账户

--- app/models/account.js
+++ app/models/account.js
@@ -3,9 +3,10 @@ const Schema = mongoose.Schema
 
 // 账号模板
 const AccountSchema = new Schema({
-    name: { type: String, default: null }, // 账号名
+    account: { type: String, default: null }, // 账号名
     password: { type: String, default: null }, // 密码
     user: { type: Schema.Types.ObjectId, ref: 'user' }, // 用户
+    type: { type: Number, default: 1 }, // 用户类型 0 管理员，1 一般用户
     createTime: { type: Date, default: Date.now }, // 创建时间
     is_deleted: { type: Number, default: 1 } // 删除状态
 })
--- app/routes/user/index.js
+++ app/routes/user/index.js
@@ -1,10 +1,14 @@
 import Router from 'koa-router'
 import user from './user'
+import account from './account'
 
 const router = Router()
 
 // 用户
 router.use('/user', user.routes(), user.allowedMethods())
+
+// 账户
+router.use('/user', account.routes(), account.allowedMethods())
 
 export default router
 
