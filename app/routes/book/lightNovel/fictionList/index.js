5a68b02d7a3cfef3920d981aaaf109ee55c0db9b
蒋亚林 <18725944157@163.com>
2018-04-18 09:56:48 +0800

    x'x

--- app/routes/book/lightNovel/fictionList/index.js
+++ app/routes/book/lightNovel/fictionList/index.js
@@ -11,7 +11,7 @@ router.get('/', async (ctx, next) => {
     const qs = new RegExp('') // 标题正则参数
     const populate = [{ path: 'area', select: 'name' }, { path: 'library', select: 'name' }, { path: 'cover' }]
     const criteria = { is_deleted: 1, $or: [{ bookName: qs }, { author: qs }] } // 查询条件
-    const fields = { bookName: 1, area: 1, library: 1, author: 1, illustrator: 1, introduction: 1, cover: 1 } // 待返回的字段
+    const fields = { bookName: 1, area: 1, library: 1, author: 1, illustrator: 1, introduction: 1, cover: 1, file: 1 } // 待返回的字段
     const options = { sort: [{ createTime: -1 }] } // 排序
 
     const model = FictionList(page, pageSize, populate, criteria, fields, options)
