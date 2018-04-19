3c3888c96dfc03aa758cb28e63d68e9105c4d10e
蒋亚林 <18725944157@163.com>
2018-04-19 10:38:10 +0800

    点击统计-卷

---
+++ app/routes/basis/statistics/click/index.js
@@ -0,0 +1,70 @@
+import Router from 'koa-router'
+import VolumeInfo from './../../../book/volume/volumeInfo/model'
+import FictionInfo from './../../../book/lightNovel/fictionInfo/model'
+import StatisticsInfo from './../../../basis/statistics/statisticsInfo/model'
+import StatisticsEdit from './../../../basis/statistics/statisticsEdit/model'
+
+const router = Router()
+
+// 统计点击
+router.get('/:id/:type', async (ctx, next) => {
+    const id = ctx.params.id
+    const type = ctx.params.type
+
+    const criteria = { is_deleted: 1, $or: [{ _id: id }] } // 查询条件
+    const populate = []
+    const fields = { statistics:1 } // 待返回的字段
+    const options = { sort: [{ createTime: -1 }] } // 排序
+
+    let statisticsId = ''
+    if (type === 'volume') {
+        const model = VolumeInfo(criteria, fields, options, populate)
+
+        const volumeInfo = await model.then((resolve) => {
+            return resolve
+        }).catch((reject) => {
+            return reject
+        })
+
+        statisticsId = volumeInfo.data.statistics
+    } else if (type === 'book') {
+        const model = FictionInfo(criteria, fields, options, populate)
+
+        const fictionInfo = await model.then((resolve) => {
+            return resolve
+        }).catch((reject) => {
+            return reject
+        })
+
+        statisticsId = fictionInfo.data.statistics
+    }
+
+    // 查询点击纪录
+    const criteria0 = { is_deleted: 1, $or: [{ _id: statisticsId }] } // 查询条件
+    const populate0 = []
+    const fields0 = { click: 1 } // 待返回的字段
+    const options0 = { sort: [{ createTime: -1 }] } // 排序
+
+    const model0 = StatisticsInfo(criteria0, fields0, options0, populate0)
+
+    const statisticsInfo = await model0.then((resolve) => {
+        return resolve
+    }).catch((reject) => {
+        return reject
+    })
+
+    // 编辑统计点击
+    const criteria1 = { is_deleted: 1, _id: statisticsId } // 查询条件
+    const options1 = { sort: [{ createTime: -1 }] } // 排序
+
+    const model1 = StatisticsEdit(criteria1, { click: statisticsInfo.data.click + 1 }, options1)
+
+    ctx.body = await model1.then((resolve) => {
+        return resolve
+    }).catch((reject) => {
+        return reject
+    })
+
+})
+
+export default router
--- app/routes/basis/statistics/index.js
+++ app/routes/basis/statistics/index.js
@@ -1,7 +1,11 @@
 import Router from 'koa-router'
+import click from './click'
 import down from './down'
 
 const router = Router()
+
+// 点击
+router.use('/statistics/click', click.routes(), click.allowedMethods())
 
 // 下载
 router.use('/statistics/down', down.routes(), down.allowedMethods())
