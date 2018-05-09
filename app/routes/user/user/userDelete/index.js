b9f578fed20cc894d6e3164ec5be676c0c9cc2f0
蒋亚林 <18725944157@163.com>
2018-05-09 17:59:17 +0800

    删除用户的同时删除皮肤

--- app/routes/user/user/userDelete/index.js
+++ app/routes/user/user/userDelete/index.js
@@ -1,5 +1,6 @@
 import Router from 'koa-router'
 import UserDelete from './model'
+import SkinDelete from './../../skin/skinDelete/model'
 
 const router = Router()
 
@@ -11,13 +12,23 @@ router.delete('/', async (ctx, next) => {
     const doc = { is_deleted: 0 } // 修改的字段
     const options = { sort: [{ createTime: -1 }] } // 排序
 
-    const model = UserDelete(criteria, doc, options)
-
-    ctx.body = await model.then((resolve) => {
+    const userDelete = await UserDelete(criteria, doc, options).then((resolve) => {
         return resolve
     }).catch((reject) => {
         return reject
     })
+
+    const criteria1 = { is_deleted: 1, $or: [{ user: parameter.id }] } // 查询条件
+    const doc1 = { is_deleted: 0 } // 修改的字段
+    const options1 = { sort: [{ createTime: -1 }] } // 排序
+
+    await SkinDelete(criteria1, doc1, options1).then((resolve) => {
+        return resolve
+    }).catch((reject) => {
+        return reject
+    })
+
+    ctx.body = userDelete
 
 })
 
